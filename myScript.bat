115933aacb59cda0209d85db6052245b4c680cbe
Bhagyashree MR <101582503+bhagyashree-mr@users.noreply.github.com>
2023-12-26 11:33:48 +0530

    Update myScript.bat

--- myScript.bat
+++ myScript.bat
@@ -5,7 +5,7 @@ rem Create a Directory
 mkdir abc
 
 rem List Files with Details, Sorted by Modification Time 
-dir | Sort-Object LastWriteTime
+dir /O-D
 
 rem Sort (if you have a list of items, replace this with your actual list)
 sort
