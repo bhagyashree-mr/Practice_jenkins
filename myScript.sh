a3d27dc218b8b5c8e4005e327709bfa332c1d0de
Bhagyashree MR <101582503+bhagyashree-mr@users.noreply.github.com>
2023-12-29 14:33:23 +0530

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -1,5 +1,22 @@
-echo "Start of the script"
-echo "~~~~~~~~~~~~~~~~~~~"
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+cat -n Automobiles.txt
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+sort -f Automobiles.txt < Sorted_automobiles.txt
+
+cat -n Sorted_automobiles.txt
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+grep -ni '^a' Automobiles.txt
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+
+
+
+
+
 
 
 
