f34820aa8d3f986256c17523d83b501471257e7f
Bhagyashree MR <101582503+bhagyashree-mr@users.noreply.github.com>
2023-12-29 15:14:08 +0530

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -1,22 +1,18 @@
-
+echo "Start of the script"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
+echo "Contents of Automobiles.txt"
 cat -n Automobiles.txt
+
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+sort -f Automobiles.txt > Sorted_automobiles.txt
 
-sort -f Automobiles.txt < Sorted_automobiles.txt
+echo "Sorted contents:"
+cat Sorted_automobiles.txt
 
-cat -n Sorted_automobiles.txt
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Lines starting with 'A' or 'a':"
+grep -ni '^a' Sorted_automobiles.txt
 
-grep -ni '^a' Automobiles.txt
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-
-
-
-
-
-
-
-
-
+echo "End of the script"
